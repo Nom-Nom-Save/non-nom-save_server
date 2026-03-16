42afa1f23c7499a4eaf766144a95c40044c6a989
NureBiliakDanylo <danya.bilyak@gmail.com>
2026-03-16 06:45:56 +0200

    refactor: changes in establishments DB schema

--- src/database/schema/establishments.schema.ts
+++ src/database/schema/establishments.schema.ts
@@ -1,26 +1,31 @@
-import {
-  pgTable,
-  uuid,
-  varchar,
-  text,
-  timestamp,
-  boolean,
-  numeric,
-  integer,
-} from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, text, timestamp, boolean, numeric } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
 
 export const establishments = pgTable('establishments', {
   id: uuid('id').primaryKey().defaultRandom(),
   email: varchar('email', { length: 255 }).notNull().unique(),
-  password: varchar('password', { length: 255 }),
   isEmailVerified: boolean('is_email_verified').default(false).notNull(),
-  name: varchar('name', { length: 255 }).notNull(),
+  password: varchar('password', { length: 255 }),
+  name: varchar('name', { length: 255 }),
   description: text('description'),
-  address: text('address').notNull(),
-  latitude: numeric('latitude').notNull(),
-  longitude: numeric('longitude').notNull(),
+  address: text('address'),
+  latitude: numeric('latitude'),
+  longitude: numeric('longitude'),
   workingHours: varchar('working_hours', { length: 255 }),
   rating: numeric('rating', { precision: 3, scale: 2 }),
-  boundTo: varchar('bound_to', { length: 255 }),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
+  boundTo: varchar('bound_to', { length: 255 })
+    .default(sql`encode(gen_random_bytes(110), 'hex')`)
+    .notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+  status: varchar('status', { length: 20 })
+    .generatedAlwaysAs(
+      sql`CASE 
+      WHEN name IS NOT NULL 
+       AND address IS NOT NULL
+       AND boundTO IS NOT NULL 
+      THEN 'Active' 
+      ELSE 'Inactive' 
+    END`
+    )
+    .notNull(),
 });
