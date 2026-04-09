edd825758325aee4cc3e6fbbd554eda4b6a50579
NureBiliakDanylo <danya.bilyak@gmail.com>
2026-04-09 07:37:14 +0300

    refactor: products module general

--- src/modules/products/types/products.type.ts
+++ src/modules/products/types/products.type.ts
@@ -1,10 +1,13 @@
 import { products } from '../../../database/schema/products.schema';
 import { InferSelectModel } from 'drizzle-orm';
+import { PaginationParams } from '../../../shared/types/pagination.type';
 
 export type Product = InferSelectModel<typeof products> & {
   types?: string[];
   allergens?: string[];
 };
+
+export type RawProduct = InferSelectModel<typeof products>;
 
 export type CreateProductInput = {
   name: string;
@@ -18,3 +21,9 @@ export type CreateProductInput = {
 };
 
 export type UpdateProductInput = Partial<CreateProductInput>;
+
+export type GetProductsParams = {
+  establishmentBoundTo: string;
+  filterType?: 'Private' | 'All';
+  pagination?: PaginationParams;
+};
